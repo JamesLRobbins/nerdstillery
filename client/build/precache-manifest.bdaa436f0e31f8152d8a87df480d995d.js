5e8304611b2439c8ba4d0b5b49fcb09d2921194a
James Robbins <jameslrobbins86@gmail.com>
2020-10-26 19:09:51 -0500

    AWS Push

---
+++ client/build/precache-manifest.bdaa436f0e31f8152d8a87df480d995d.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "656ce0f9d35470f61a1adb3d73d38bbc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "32ebb7e7227e61c179d0",
+    "url": "/static/css/2.e6cf9162.chunk.css"
+  },
+  {
+    "revision": "bf6f799f88f89acf7809",
+    "url": "/static/css/main.c7928d7c.chunk.css"
+  },
+  {
+    "revision": "32ebb7e7227e61c179d0",
+    "url": "/static/js/2.4a3267cf.chunk.js"
+  },
+  {
+    "revision": "1a1a42c150bd05496b97b265c552c789",
+    "url": "/static/js/2.4a3267cf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bf6f799f88f89acf7809",
+    "url": "/static/js/main.6927c2f2.chunk.js"
+  },
+  {
+    "revision": "c6ab9abfb545b75c0280",
+    "url": "/static/js/runtime-main.8129dfe4.js"
+  },
+  {
+    "revision": "f10c3699055ee4166b5ac5b2d4e08463",
+    "url": "/static/media/Shane.f10c3699.png"
+  },
+  {
+    "revision": "527ad685a8f10d5ad27f8cceb8e2f1a1",
+    "url": "/static/media/StaticSirenLogo.527ad685.png"
+  },
+  {
+    "revision": "2bcc8b62ce2f10f38cec08a49ad152db",
+    "url": "/static/media/crypticCallingLogo.2bcc8b62.png"
+  },
+  {
+    "revision": "dbf9d11381775fb4160920bc2df77ee7",
+    "url": "/static/media/lastWords.dbf9d113.jpg"
+  },
+  {
+    "revision": "b21373b8dcdfdeec794f9db0d905b61a",
+    "url": "/static/media/nerdCouple.b21373b8.png"
+  },
+  {
+    "revision": "2b9469bcc0f44e09ce428f7879c2aac5",
+    "url": "/static/media/nerdlogo.2b9469bc.jpg"
+  },
+  {
+    "revision": "63e588e680ab514a8b038c619fc521db",
+    "url": "/static/media/segaJamesLogo.63e588e6.png"
+  },
+  {
+    "revision": "a529a619e2fc81fe3818114aefc7f453",
+    "url": "/static/media/shaneHomepageTest.a529a619.jpg"
+  },
+  {
+    "revision": "341a46ee182c67ff67dcf079541b82bf",
+    "url": "/static/media/shaneIconInterted.341a46ee.png"
+  }
+]);
