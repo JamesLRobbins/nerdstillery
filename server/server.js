02efe60f1713cc51006469ebe9b3afda1374440a
James Robbins <jameslrobbins86@gmail.com>
2020-10-28 20:37:52 -0500

    Heroku Push

--- server/server.js
+++ server/server.js
@@ -20,15 +20,13 @@ app.use(cors());
 app.use('/api', require('./routes/news'))
 app.use('/api', require('./routes/image'))
 
-if (process.env.NODE_ENV === 'production') {
-  // Serve any static files
-  app.use(express.static(path.join(__dirname, 'client/build')));
-    
-  // Handle React routing, return all requests to React app
-  app.get('*', function(req, res) {
-    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
-  });
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
 }
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname+'/client/build/index.html'));
+});
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
 
