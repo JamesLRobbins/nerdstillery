85647baa34ef5f84c109200a21a34254516c9169
James Robbins <jameslrobbins86@gmail.com>
2020-11-02 11:55:42 -0600

    Social Media Icons

--- client/src/components/Home/Contact/Contact.js
+++ client/src/components/Home/Contact/Contact.js
@@ -3,33 +3,16 @@ import { Button, Icon, Container, Grid, Divider } from 'semantic-ui-react'
 
 function Contact() {
     return(
-    //     <Container>
-    //         <Grid columns={2} className="contact">
-    //             <Grid.Row>
-    //                 <Grid.Column>
-    //                     <h4><b>Social Media</b></h4> <br />
-    //                     <a href="https://www.facebook.com/Nerdstillery/" color="facebook"><Icon name="facebook" />Facebook</a><br />
-    //                     <a href="https://www.instagram.com/Nerdstillery/" color="instagram"><Icon name="instagram" />Instagram</a><br />
-    //                     <a href="https://www.twitter.com/Nerdstillery/" color="twitter"><Icon name="twitter" />Twitter</a><br />
-    //                 </Grid.Column>
-    //                 <Grid.Column>
-    //                     <h4><b>Contact</b></h4> <br />
-    //                     <b>Website issues:</b> JamesLRobbins86@gmail.com<br />
-    //                     <b>General Questions:</b> Nerdstillery@gmail.com<br />
-    //                 </Grid.Column>
-    //             </Grid.Row>
-    //         </Grid> 
-    //    </Container> 
-
         <Container>
             <Divider section inverted/>
-            <Grid centered className="contact">
+            <Grid centered className="contact" >
                 <Grid.Row columns={1}>
                     <Grid.Column fluid verticalAlign="middle">
                     <h4><b>Social Media</b></h4> <br />
-                        <Button color="facebook" size="tiny"><a href="https://www.facebook.com/Nerdstillery/" color="facebook"><Icon name="facebook" />Facebook </a></Button>  
-                        <Button color="instagram" size="tiny"><a href="https://www.instagram.com/Nerdstillery/" color="instagram"><Icon name="instagram" />Instagram </a></Button>
-                        <Button color="twitter" size="tiny"><a href="https://www.twitter.com/Nerdstillery/" color="twitter"><Icon name="twitter" />Twitter</a></Button>
+                        <a href="https://www.facebook.com/Nerdstillery/"><Icon name="facebook" size="large"/></a>
+                        <a href="https://www.instagram.com/Nerdstillery/"><Icon name="instagram" size="large"/></a>
+                        <a href="https://www.twitter.com/Nerdstillery/"><Icon name="twitter" size="large"/></a>
+                        <a href="https://www.linkedin.com/in/james-robbins-08750953/"><Icon name="linkedin" size="large"/></a>
                         </Grid.Column>
                     <Grid.Column fluid verticalAlign="middle">
                         <br />
--- client/src/components/Shane/Contact/Contact.js
+++ client/src/components/Shane/Contact/Contact.js
@@ -9,12 +9,8 @@ function Contact() {
                 <Grid.Row columns={1}>
                     <Grid.Column fluid verticalAlign="middle">
                     <h4><b>Social Media</b></h4> <br />
-                    <Button color='facebook' size="tiny">
-                        <Icon name='facebook' /> <a href="https://www.facebook.com/SidneyVinsonPhotography/">Facebook</a>
-                    </Button>  
-                    <Button color='instagram' size="tiny">
-                        <Icon name='instagram' /> <a href="https://www.instagram.com/sidneyvinsonphotography/">Instagram</a>
-                    </Button>
+                        <a href="https://www.facebook.com/SidneyVinsonPhotography/"><Icon name='facebook' size="large"/></a>
+                        <a href="https://www.instagram.com/sidneyvinsonphotography/"><Icon name='instagram' size="large"    /></a>
                         </Grid.Column>
                     <Grid.Column fluid verticalAlign="middle">
                         <br />
