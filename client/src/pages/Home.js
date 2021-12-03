4dae6b016be9c902743e3644e15f1ab1d3146e29
James Robbins <jameslrobbins86@gmail.com>
2021-12-03 15:18:09 -0600

    Fix Logo

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -3,25 +3,32 @@ import MainNav from '../components/Home/MainNav/MainNav';
 import Logo from '../components/Home/Logo/Logo';
 import About from '../components/Home/About/About';
 import Contact from '../components/Home/Contact/Contact';
+import { Grid } from 'semantic-ui-react'
 
 
 export default class Home extends React.Component {
 
     render() {
         return( 
-        <div>   
-            <MainNav 
-                about = {this.onClick=this.toggleAbout}
-                contact = {this.onClick=this.toggleContact}
-            />
-                <div className="ui container">
-                    <Logo 
-                        home = {<div onClick={this.toggleAbout}><img src={require("../images/nerdlogo.jpg")} alt="nerdstillery" className="ui image nerdLogo"/></div>}
-                    />
-                </div>
+        <Grid>   
+            <Grid.Row>
+                <MainNav 
+                    about = {this.onClick=this.toggleAbout}
+                    contact = {this.onClick=this.toggleContact}
+                /> 
+            </Grid.Row>
+                <Grid.Row centered>
+                    <Logo home = {<div onClick={this.toggleAbout}><img src={require("../images/nerdlogo.jpg")} alt="nerdstillery" className="ui image nerdLogo"/></div>}/>
+                </Grid.Row>
+            <Grid.Row centered>
                 <About />
+            </Grid.Row>
+
+            <Grid.Row centered>
                 <Contact />  
-        </div>
+            </Grid.Row>
+                
+        </Grid>
         )
     }
 }
